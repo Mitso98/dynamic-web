36375dc7a434c66ec4e56975768e2decd4b59a50
Mosatfa Abdelghany <mostafa.ahmed19293949@gmail.com>
2022-01-08 21:14:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+pure javaScript responsive page 
---
+++ js/app.js
@@ -0,0 +1,142 @@
+// seprate content from html
+let article = [
+  {
+    title: "Article",
+    content: `        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci.
+        orem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci
+        orem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci
+        `,
+  },
+  {
+    title: "Article",
+    content: `        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci.`,
+  },
+  {
+    title: "Article",
+    content: `        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci.`,
+  },
+  {
+    title: "Article",
+    content: `        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci.`,
+  },
+  {
+    title: "Article",
+    content: `        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci.`,
+  },
+  {
+    title: "Article",
+    content: `        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
+        optio commodi delectus consequuntur dicta, adipisci eum velit aliquam
+        perferendis fugiat. Eveniet beatae natus quae fugit tempore illo, hic
+        magnam provident doloribus labore ducimus itaque, eligendi reprehenderit
+        sint laborum adipisci a vitae possimus maxime temporibus dignissimos
+        eius dolor laudantium! Aut culpa deleniti sunt ipsa! Quae omnis voluptas
+        velit et recusandae modi laudantium facilis facere atque! Accusamus
+        labore voluptatibus adipisci sequi fugiat hic non ut, quibusdam sint est
+        magni cumque repudiandae quia doloremque cupiditate perspiciatis
+        blanditiis libero reprehenderit consequatur et officia atque quas. Dicta
+        possimus natus temporibus soluta error illo vero adipisci.`,
+  },
+];
+
+// fetch data
+const section = document.getElementsByClassName("articles-section")[0];
+const navUl = document.getElementById("nav-ul");
+const style = `style="margin-left:auto;`;
+
+article.map((obj, index) => {
+  // build buttons
+  navUl.innerHTML += `<li><button class="nav-btn" id="${index}">${obj.title} ${
+    index + 1
+  }</button></li>`;
+  // build articles
+  section.innerHTML += `
+    <article id="article-${index}" ${(index+1) % 2 === 0 ? style : ""}">
+    <h3 class="article-title">${obj.title} ${index + 1}</h3>
+    <p>
+        ${obj.content}
+    </p>
+    </article>
+`;
+});
+
+// add listners to buttons
+for (let i = 0; i < article.length; i++) {
+  document
+    .getElementById(`${i}`)
+    .addEventListener("click", () =>
+      document
+        .getElementById(`article-${i}`)
+        .scrollIntoView({ behavior: "smooth", block: "start" })
+    );
+}
